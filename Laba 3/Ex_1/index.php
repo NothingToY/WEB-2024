5a7c2a67aa7226ef3c3135bb6ff9b49fc0a4722a
Андрей <andersen2004@list.ru>
2024-03-27 21:06:38 +0200

    Ex_1(b) Regular Expressions

--- Laba 3/Ex_1/index.php
+++ Laba 3/Ex_1/index.php
@@ -1,13 +1,23 @@
 <?php
 
 echo "Task 1a \n";
-
 $str1 = 'ahb acb aeb aeeb adcb axeb';
 $pattern1 = '/a..b/';
 $result1 = preg_match_all($pattern1, $str1, $matches);
-
 echo "Строчки, соответствующие шаблону:\n";
 print_r($matches[0]);
-
 echo "\n";
-
+echo "Task 1b \n";
+$str2 = 'a1b2c3';
+echo "Строка до преобразования " .$str2 ."\n";
+$patterns2 = [];
+$patterns2[0] = '/1/';
+$patterns2[1] = '/2/';
+$patterns2[2] = '/3/';
+$replacements = [];
+$replacements[0] = '1';
+$replacements[1] = '4';
+$replacements[2] = '9';
+$result2 = preg_replace($patterns2, $replacements, $str2);
+$result2 = preg_replace_callback('/\d/', function($num) {return $num[0]**3;}, $str2);
+echo "После преобразования " .$result2 ."\n";
