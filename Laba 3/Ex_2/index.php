bd68203a6eb9f57233fada611d906af7c1bbfb3d
Андрей <andersen2004@list.ru>
2024-03-28 18:16:52 +0200

    Ex_2(b) Rework

--- Laba 3/Ex_2/index.php
+++ Laba 3/Ex_2/index.php
@@ -3,12 +3,21 @@ session_start();
 if ("POST"==$_SERVER["REQUEST_METHOD"])
 {
     $surname = $_POST["surname"];
+if ($_SERVER["REQUEST_METHOD"]=="POST"){
     $name = $_POST["name"];
+    $surname = $_POST["surname"];
     $age = $_POST["age"];
 
     $_SESSION["surname"] = $surname;
     $_SESSION["name"] = $name;
     $_SESSION["age"] = $age;
+
+
+
+    $salary = $_POST["salary"];
+    $nationality = $_POST["nationality"];
+    $userData = ['name'=>$name, 'age'=>$age, 'surname'=>$surname, 'salary'=>$salary, 'nationality'=>$nationality];
+    $_SESSION['userData'] = $userData;
 
     header("Location: disp.php");
     exit();
@@ -20,38 +29,13 @@ if ("POST"==$_SERVER["REQUEST_METHOD"])
 <html lang="en">
 
 <head>
-    <title>Word and Character Сounter</title>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>
         Form
     </title>
 </head>
-
 <body>
-    <form method="post">
-        <label>
-            <textarea name="text" rows="4" cols="40"></textarea><br>
-        </label>
-        <input type="submit" name="submit" value="Посчитать">
-    </form>
-
-<?php
-
-if(isset($_POST['submit']) and (!empty($_POST['text'])))
-    {
-    $text = $_POST['text'];
-    $word_count = str_word_count($text);
-    $char_count = strlen($text);
-    echo "<br> Кол-во слов тексте: $word_count </br>";
-    echo "Кол-во букв тексте: $char_count";
-    }
-
-else
-    {
-        echo "Напишите и посчитайте !";
-    }
-?>
     <form method="post" action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]); ?>">
         <label for="surname">Фамилия:</label>
         <input type="text" id="surname" name="surname" required><br>
@@ -62,5 +46,4 @@ else
         <input type="submit" value="Отправить данные">
     </form>
 </body>
-
 </html>
